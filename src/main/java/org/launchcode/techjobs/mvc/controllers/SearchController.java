41ef337e67e29633aabe14219b2da5e482b7be57
Your Name <you@example.com>
2022-11-04 14:07:10 -0500

    fix thyme leaf and css

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -15,6 +21,7 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @RequestMapping("search")
 public class SearchController {
 
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
@@ -22,5 +29,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm){
+              ArrayList<Job> filteredJobs;
+
+        if(searchTerm.equals("all") || searchTerm.equals("")) {
+            filteredJobs = JobData.findAll();
+
+        } else {
+            filteredJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            System.out.print(searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobPosts", filteredJobs);
+        return "search";
+    }
+
 
 }
--- src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
+++ src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
@@ -230,5 +230,6 @@ public class JobData {
         return allCoreCompetency;
     }
 
+
 }
 
